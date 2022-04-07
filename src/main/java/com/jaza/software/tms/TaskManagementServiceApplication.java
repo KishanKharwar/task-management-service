1220ca2bd4f06f2b21e22e68d6596621e1a13d23
Kishan Kumar Kharwar <KishanK@quinnox.com>
2022-04-07 21:26:54 +0530

    removed unused commented code

--- src/main/java/com/jaza/software/tms/TaskManagementServiceApplication.java
+++ src/main/java/com/jaza/software/tms/TaskManagementServiceApplication.java
@@ -5,16 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskManagementServiceApplication  {
-//extends SpringBootServletInitializer implements CommandLineRunner
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(TaskManagementServiceApplication.class);
-//	}
-//
-//	@Override
-//	public void run(String... args) throws Exception {
-//		System.out.println("Application Started !!");
-//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagementServiceApplication.class, args);
